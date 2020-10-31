3c5427297fa98adc4e1e9a30a2a634f64c042191
Taylor Booth <taylorkbooth@gmail.com>
2020-10-31 15:18:49 -0400

    commit tree 1169cd4bdc3c0c69a3111f80b454064c799700ee

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(people)
+    badges=[]
+    people.each do |attendees|
+        badges << "Hello, my name is #{attendees}."
+    end
+    badges
+end
+
+def assign_rooms(people)
+    room_assignments=[]
+    people.each_with_index do |attendee, index|
+        room_assignments << "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
+    end
+    room_assignments
+end
+
+def printer(people)
+    batch_badge_creator(people).each { |badge_text| puts badge_text}
+    assign_rooms(people).each { |assignments| puts assignments }
+end
